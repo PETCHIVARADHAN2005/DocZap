2c249016d857223a48962aa10849bf29d5e47bd2
PETCHIVARADHAN2005 <lpetchi2005@gmail.com>
2025-02-04 22:08:45 +0530

    configuration of mongodb is updated

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -4,14 +4,7 @@ const connectDB = async () => {
 
     mongoose.connection.on('connected', ()=> console.log("Database Connected"))
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/DocZap`,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        // If using MongoDB Atlas, you might need:
-        retryWrites: true,
-        w: 'majority'
-    }).then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('MongoDB connection error:', err));
+    await mongoose.connect(`${process.env.MONGODB_URI}/DocZap`);
 }
 
 export default connectDB
