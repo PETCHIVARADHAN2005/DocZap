9d0150f5f69a4ff694222acbd30abb110d9a3e44
PETCHIVARADHAN2005 <lpetchi2005@gmail.com>
2025-02-04 22:30:28 +0530

    mongodb connection updated in backend

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -5,12 +5,11 @@ const connectDB = async () => {
     mongoose.connection.on('connected', ()=> console.log("Database Connected"))
 
     await mongoose.connect(`${process.env.MONGODB_URI}/DocZap`,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            authSource: 'admin',
-            ssl: true,
-            sslValidate: true
-          });
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        authSource: 'admin',
+        ssl: true
+      });
 }
 
 export default connectDB
